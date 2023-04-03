559729b2fe274910e9f4d46c92a1762ac17da23d
NKofR <ignacioclementti@gmail.com>
2023-04-03 04:35:51 -0300

    Commit inicial

---
+++ ejercicio2/collapse.js
@@ -0,0 +1,6 @@
+const idown = document.querySelector("#idown");
+const pdown = document.querySelector("#pdown");
+
+idown.addEventListener("click", (e) => {
+    p.classList.toggle(`collapsed`);
+});
