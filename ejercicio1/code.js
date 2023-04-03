c776a75fe85828b7c631960f6a9da8b2c4ff9e8f
NKofR <ignacioclementti@gmail.com>
2023-04-03 06:32:05 -0300

    agregando ejercicio 3

--- ejercicio1/code.js
+++ ejercicio1/code.js
@@ -4,3 +4,15 @@ const links = document.querySelector("#links");
 bMore.addEventListener("click", (e) => {
     links.classList.toggle(`collapsed`);
 });
+
+const nombre = "Miguel"
+
+const saludo = () => {
+    let a = `Hola ${nombre} , cómo estás?`;
+console.log(a)
+}
+
+saludo();
+
+let b = "Chancho qliao";
+console.log(b);
